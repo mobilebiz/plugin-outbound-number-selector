98d826383e3c781d395f9715bb2b8b86a2245c46
Katsumi Takahashi <katsumi@gij.com>
2022-07-22 10:36:07 +0900

    Outbound sip marged.

--- src/actions/OutboundNumberSelector/OutboundNumberSelector.js
+++ src/actions/OutboundNumberSelector/OutboundNumberSelector.js
@@ -3,6 +3,8 @@ import { Manager } from '@twilio/flex-ui';
 export function replaceFromNumber(flex) {
   console.info(`🐸 Replace caller number`);
 
+  // eslint-disable-next-line no-undef
+  const { REACT_APP_SIP_DOMAIN } = process.env;
   const manager = Manager.getInstance();
 
   flex.Actions.replaceAction('StartOutboundCall', (payload, original) => {
@@ -15,13 +17,21 @@ export function replaceFromNumber(flex) {
         manager.store.getState()['outbound-number-selector'].DefaultNumber
           .defaultNumber;
       if (!callerId) reject('Default number not set.');
+
       resolve(
         manager.store.getState()['outbound-number-selector'].DefaultNumber
           .defaultNumber,
       );
     }).then((callerId) => {
+      let destination = payload.destination || '';
+      if (!destination.match(/^\+/)) {
+        destination = `sip:${destination}@${REACT_APP_SIP_DOMAIN}`;
+      }
+      console.log(`🐞 destination: ${destination}`);
+
       original({
         ...payload,
+        destination,
         callerId,
       });
     });
